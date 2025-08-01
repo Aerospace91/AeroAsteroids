bc5afe6c7716d342ee9fa5dfb55a37f5454589b7
Aerospaced91 <aerospaceosrs@gmail.com>
2025-08-01 14:24:36 -0700

    Initial Boilerplate with Constants

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
+import pygame
+from constants import *
+
 def main():
-    print("Hello from aeroasteroids!")
+    print('Starting Asteroids!')
+    print(f'Screen width: {SCREEN_WIDTH}')
+    print(f'Screen height: {SCREEN_HEIGHT}')
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
