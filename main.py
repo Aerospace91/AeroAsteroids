76c6021ff8d4af9ba21e40a5b2f92b68aad4ae4a
Aerospace91 <beenters@gmail.com>
2025-08-04 13:09:45 -0700

    Added Collission

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 import pygame
 from constants import *
 from player import Player
@@ -36,6 +37,11 @@ def main():
                 return
             
         updatable.update(dt)
+        
+        for obj in asteroids:
+            if obj.collides(player):
+                print("Game over!")
+                sys.exit()
         
         screen.fill('black')
         
