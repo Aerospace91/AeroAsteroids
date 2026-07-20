1c18baabf916a78c54cb519feb829d57636144fa
Aerospace91 <beenters@gmail.com>
2026-07-20 14:04:04 -0700

    Added Player Class and Draw

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
+from player import Player
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
@@ -11,6 +12,8 @@ def main():
     clock = pygame.time.Clock()
     dt = 0.0
     
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    
     while True:
         log_state()
         for event in pygame.event.get():
@@ -19,6 +22,7 @@ def main():
             
         screen.fill("black")
         dt = clock.tick(60) / 1000
+        player.draw(screen)
         pygame.display.flip()
 
 
